f441595efda1eb7758277bf22448b60e59ace28b
MOHAMMAD AFNAN HASEEB <afnan.cal@gmail.com>
2019-01-02 17:37:10 +0530

    Update HomeController.java

--- src/main/java/com/afnan/harimitti/controller/HomeController.java
+++ src/main/java/com/afnan/harimitti/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.afnan.harimitti.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
